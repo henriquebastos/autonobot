1c970470f40addb68d7254a1f37a87d11a6814bb
Henrique Bastos <henrique@bastos.net>
2017-12-05 22:50:02 -0200

    Change HEROKU_ to DOMAIN_

--- autonobot/cli.py
+++ autonobot/cli.py
@@ -60,7 +60,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=5000,
                           url_path=TOKEN)
-    updater.bot.set_webhook(config('HEROKU_URL') + TOKEN)
+    updater.bot.set_webhook(config('DOMAIN_URL') + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
